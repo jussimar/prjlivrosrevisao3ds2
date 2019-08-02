96bc5149f3871ac89dae4dac5a4ac68b2ace9403
jussimar <jussimar@jussimarleal.com.br>
2019-08-03 04:53:52 +0900

    ação de enviar\(cadastrar\) dados para o banco do servidor

--- www/components/acao.js
+++ www/components/acao.js
@@ -1,2 +1,25 @@
 // This is a JavaScript file
-
+$(document).on("click","#salvar",function(){
+    var parametros = {
+      "livro":$("#titulo").val(),
+      "autor":$("#autor").val(),
+      "ano":$("#ano").val()
+    }
+    // url:"https://dominioappsjussa.000webhostapp.com/cadastra.php",//para onde enviar
+    $.ajax({
+        type:"post", //como enviar
+        url:"https://appmobile3i2.000webhostapp.com/cadastra.php",//para onde enviar
+        data:parametros,//o que enviar
+        //se der certo
+        success: function(data){
+            navigator.notification.alert("msg:"+data);
+           $("#titulo").val(""),
+           $("#autor").val(""),
+           $("#ano").val("")
+        },
+        //se der errado
+        error: function(data){
+             navigator.notification.alert(data);
+        }
+    });    
+});
