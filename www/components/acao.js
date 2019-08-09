331e2ba3da3bf6c20798382c3f2227cb03617ee6
jussimar <jussimar@jussimarleal.com.br>
2019-08-10 04:32:34 +0900

    lista e remoção de registros

--- www/components/acao.js
+++ www/components/acao.js
@@ -43,3 +43,73 @@ $(document).on("click","#novo",function(){
 $(document).on("click","#cancelar",function(){
   desabilita();
 });
+
+
+$(document).on("click","#listar",function(){
+    $(location).attr("href","lista.html");
+});
+
+function preencherLista(){
+  var itemlista = "";
+  $.ajax({
+        type:"post", //como enviar
+        url:"https://appmobile3i2.000webhostapp.com/listar.php",//para onde enviar
+        dataType:'json',//o que enviar
+        //se der certo
+        success: function(data){
+            $.each(data.livros, function(i, dados){
+              itemlista+="<option value='"+dados.codigo+"'>"+dados.titulo+"</option>";
+            });
+            $("#listalivros").html(itemlista);
+        },
+        //se der errado
+        error: function(data){
+             navigator.notification.alert(data);
+        }
+    });    
+}
+
+$(document).on("change","#listalivros",function(){
+  var parametro ={
+    "codigo":$("option:selected",("#listalivros")).val()
+  }
+  
+  $.ajax({
+        type:"post", //como enviar
+        url:"https://appmobile3i2.000webhostapp.com/listarUm.php",//para onde enviar
+        data:parametro,
+        dataType:'json',//o que enviar
+        //se der certo
+        success: function(data){
+           $("#codigo").val(data.livro.codigo);
+           $("#titulo").val(data.livro.titulo);
+           $("#autor").val(data.livro.autor);
+           $("#ano").val(data.livro.ano);
+        },
+        //se der errado
+        error: function(data){
+             navigator.notification.alert(data);
+        }
+    });
+});
+
+$(document).on("click","#deletar",function(){
+  var parametro ={
+    "codigo":$("#codigo").val()
+    
+  }
+ 
+  $.ajax({
+        type:"post", //como enviar
+        url:"https://appmobile3i2.000webhostapp.com/deletar.php",//para onde enviar
+        data:parametro,
+        //se der certo
+        success: function(data){
+           location.reload();
+        },
+        //se der errado
+        error: function(data){
+             navigator.notification.alert(data);
+        }
+    });
+});
